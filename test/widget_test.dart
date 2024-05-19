01fee7df1ca99717a812b77bb1a9637658064112
Samuel Arandia <samuel.arandia@urosario.edu.co>
2024-05-19 11:15:20 -0500

    otro commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:happybills_branch_juanrojas/app.dart';
 
 import 'package:happybills_branch_juanrojas/main.dart';
 
