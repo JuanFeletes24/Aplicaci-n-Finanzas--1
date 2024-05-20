92a1450129da75c1a6edd8dd034dba289b92a9e6
AndresC103 <andresju.castano@urosario.edu.co>
2024-05-19 22:04:36 -0500

    Avance Backing

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:happybills_branch_juanrojas/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:happybills_branch_juanrojas/screen/auth/welcome_screen.dart';
 
 import 'screen/views/home_screen.dart';
 
@@ -8,19 +11,23 @@ class MyAppView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Happy Bills",
-      theme: ThemeData(
-        colorScheme: const ColorScheme.light(
-          background: Colors.white,
-          onBackground: Colors.black,
-          primary: Colors.blue,
-          secondary: Color.fromARGB(255, 89, 135, 214),
-          tertiary: Colors.blueGrey,
-          outline: Colors.grey
-        )
-      ),
-      home: HomeScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: "Happy Bills",
+        theme: ThemeData(
+            colorScheme: const ColorScheme.light(
+                background: Colors.white,
+                onBackground: Colors.black,
+                primary: Colors.blue,
+                secondary: Color.fromARGB(255, 89, 135, 214),
+                tertiary: Colors.blueGrey,
+                outline: Colors.grey)),
+        home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+            builder: (context, state) {
+          if (state.status == AuthenticationStatus.authenticated) {
+            return const HomeScreen();
+          } else {
+            return const WelcomeScreen();
+          }
+        }));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:happybills_branch_juanrojas/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:happybills_branch_juanrojas/simple_bloc_observer.dart';
+import 'package:user_repository/user_repository.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  Bloc.observer = SimpleBlocObserver();
+  runApp(MyApp(FirebaseUserRepo()));
 }
-
